c29c23a9f89aed62d0bdb6fed6abcffaf60a0b79
James <jacopoberton98@gmail.com>
2025-09-08 23:57:37 +0200

    Update sb-config.js

--- assets/sb-config.js
+++ assets/sb-config.js
@@ -1,3 +1,3 @@
 // Duplicate as sb-config.js and paste your Supabase URL + anon key.
 export const SUPABASE_URL = "https://sfmapuaykieonzsmxdak.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmbWFwdWF5a2llb256c214ZGFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczMzYwODMsImV4cCI6MjA3MjkxMjA4M30.WyGquAUjsllIYzSP2F19LcbkDYKv-wDxsbo5MFCUFyM";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmbWFwdWF5a2llb256c214ZGFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNjg1NjEsImV4cCI6MjA3Mjk0NDU2MX0.QBrzd07Gh7UYh-yRGstu4cQ8q8ItifPAbUxbjcL1r5g";
