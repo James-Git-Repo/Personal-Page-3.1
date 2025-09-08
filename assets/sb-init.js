d4846d05598b2e1ae333e000aa4bc480d080b576
James <jacopoberton98@gmail.com>
2025-09-08 23:21:13 +0200

    Update sb-init.js

--- assets/sb-init.js
+++ assets/sb-init.js
@@ -1,4 +1,11 @@
+// assets/sb-init.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./sb-config.js";
-export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-window.$sb = sb;
+
+export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: { persistSession: false }
+});
+
+// Debug helpers (so we can verify in the console)
+window.__SB = sb;
+window.__SB_INFO__ = { url: SUPABASE_URL };
